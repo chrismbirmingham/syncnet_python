75e1a27de36095f6419b6771776c7488e47ece33
chrismbirmingham <cmbirmingham19@gmail.com>
2021-03-02 09:37:47 -0800

    Run individual pipeline

---
+++ run_pipeline_components.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+VIDEO="/media/chris/M2/1-Raw_Data/Videos/11/cropped/left.mp4"
+REF="11l"
+DATA="/media/chris/M2/2-Processed_Data/syncnet_output"
+# echo "Start Pipeline"
+# python run_pipeline.py --videofile $VIDEO --reference $REF --data_dir $DATA
+# echo "Start syncnet"
+# python run_syncnet.py --videofile $VIDEO --reference $REF --data_dir $DATA
+echo "Start visualize"
+python run_visualise.py --videofile $VIDEO --reference $REF --data_dir $DATA
