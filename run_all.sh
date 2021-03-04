4a25215f7017650b8bbb3a07e9c04f386bbb9d7c
chrismbirmingham <cmbirmingham19@gmail.com>
2021-03-03 18:16:27 -0800

    Set scripts for whole dataset runs

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,16 @@
 #!/bin/bash 
-VIDEO="/media/chris/M2/1-Raw_Data/Videos/1/cropped/left.mp4"
-REF="1l"
-DATA="/media/chris/M2/2-Processed_Data/syncnet_output"
-#echo "Start Pipeline"
-#python run_pipeline.py --videofile $VIDEO --reference $REF --data_dir $DATA
-echo "Start syncnet"
-python run_syncnet.py --videofile $VIDEO --reference $REF --data_dir $DATA
-echo "Start visualize"
-python run_visualise.py --videofile $VIDEO --reference $REF --data_dir $DATA
+# A simple example of the full pipeline for many videos
+
+for study_idx in {1..27}; do
+    VIDEO="/media/chris/M2/1-Raw_Data/Videos/${study_idx}/cropped/center.mp4"
+    REF="${study_idx}c"
+    DATA="/media/chris/M2/2-Processed_Data/syncnet_output"
+    echo "Start Pipeline"
+    python run_pipeline.py --videofile $VIDEO --reference $REF --data_dir $DATA
+    echo "Start syncnet"
+    python run_syncnet.py --videofile $VIDEO --reference $REF --data_dir $DATA --batch_size 50
+    echo "Start visualize center"
+    python run_visualise.py --videofile $VIDEO --reference $REF --data_dir $DATA
+  
+done
+
--- run_pipeline_components.sh
+++ run_pipeline_components.sh
@@ -3,9 +3,9 @@
 VIDEO="/media/chris/M2/1-Raw_Data/Videos/11/cropped/left.mp4"
 REF="11l"
 DATA="/media/chris/M2/2-Processed_Data/syncnet_output"
-# echo "Start Pipeline"
-# python run_pipeline.py --videofile $VIDEO --reference $REF --data_dir $DATA
-# echo "Start syncnet"
-# python run_syncnet.py --videofile $VIDEO --reference $REF --data_dir $DATA
+echo "Start Pipeline"
+python run_pipeline.py --videofile $VIDEO --reference $REF --data_dir $DATA
+echo "Start syncnet"
+python run_syncnet.py --videofile $VIDEO --reference $REF --data_dir $DATA --batch_size 20
 echo "Start visualize"
 python run_visualise.py --videofile $VIDEO --reference $REF --data_dir $DATA
